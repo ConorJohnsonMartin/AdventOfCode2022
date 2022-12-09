5857009dc89d1ead1b5e81ce382081c934b186c3
Conor Johnson Martin <71397298+ConorJohnsonMartin@users.noreply.github.com>
2022-12-09 18:17:28 -0500

    Update Day3_2.py

--- Day3/Day3_2.py
+++ Day3/Day3_2.py
@@ -1,11 +1,22 @@
 if __name__ == '__main__':
     count = 0
-    compartment1 = []
-    compartment2 = []
+    my_list = []
+    temp_list = []
 
-    with open('Day3.txt', 'r') as infile:
-        lines = [line for line in infile][:3]
-        for line in lines:
-            line.strip()
-        print(lines)
+    file = open('Day3.txt', 'r')
+    for line in file:
+        x = str(line.strip())
+        my_list.append(x)
+    for i in range(100):
+        temp_list = my_list[:3]
+        for char in temp_list[0]:
+            if char in temp_list[1] and char in temp_list[2]:
+                if char.isupper():
+                    count = count + ord(char) - 38
+                elif char.islower():
+                    count = count + ord(char) - 96
+                break
+        del my_list[:3]
+        temp_list.clear()
+
     print("The sum of the priorities is:", count)
